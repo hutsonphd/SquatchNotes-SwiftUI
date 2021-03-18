7427669d15ea6892cfe85de82c49d30ccea4a301
Hutson <github@htsn.us>
2021-03-18 11:10:45 -0500

    Initial Commit

---
+++ Shared/SquatchNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquatchNotesApp.swift
+//  Shared
+//
+//  Created by Hutson on 3/18/21.
+//
+
+import SwiftUI
+
+@main
+struct SquatchNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
